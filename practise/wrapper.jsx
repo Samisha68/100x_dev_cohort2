f4bdd75ae6bb6dd6e8548c523e7ee5aa2057b9e5
Samisha <108931301+Samisha68@users.noreply.github.com>
2024-07-23 07:15:14 +0530

    Create wrapper.jsx
    Creating a Wrapper class in React.js

---
+++ practise/wrapper.jsx
@@ -0,0 +1,16 @@
+
+function App(){
+  return <div>
+  <CardWrapper innerComponent={<TextComponent/>} /></div>
+}
+
+function CardWrapper({innerComponent}){
+  return <div style={{border: "2px solid black"}}>{innerComponent}</div>
+}
+
+function TextComponent(){
+  return   <div>hi there</div>
+}
+
+export default App;
+
