2aba5aaba08d9febe486b200c912993016e834d3
Samisha <108931301+Samisha68@users.noreply.github.com>
2024-07-26 21:22:05 +0530

    Create App.jsx
    Understood about Recoil

---
+++ Week-7-Recoil/App.jsx
@@ -0,0 +1,43 @@
+
+import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
+import { countAtom } from "./Components/store/Count";
+
+function App(){
+  return(
+    <div>
+      <RecoilRoot>
+        <Count />
+      </RecoilRoot>
+      
+    </div>
+  )
+}
+
+function Count({setCount}){
+  return(
+    <div>
+      <CountRenderer/>
+      <Buttons setCount={setCount}/>
+    </div>
+  )
+}
+
+function CountRenderer(){
+  const count=useRecoilValue(countAtom);
+  return(
+    <div>
+      {count}
+    </div>
+  )
+}
+function Buttons(){
+  const [count,setCount]=useRecoilState(countAtom);
+  return(
+    <div>
+      <button onClick={()=>{setCount(count+1)}}>Increase</button>
+      <button onClick={()=>{setCount(count-1)}}>Decrease</button>
+    </div>
+  )
+}
+
+export default App;
