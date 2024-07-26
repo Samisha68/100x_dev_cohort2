e8f65c064793b98929c22e2af585c28387ea688f
Samisha <108931301+Samisha68@users.noreply.github.com>
2024-07-26 22:55:28 +0530

    Added selectors

--- Week-7-Recoil/store/atoms/Count.jsx
+++ Week-7-Recoil/store/atoms/Count.jsx
@@ -1,6 +1,15 @@
 import {atom} from "recoil";
+import { selector } from "recoil";
 
 export const countAtom=atom({
     key: "countAtom",
     default: 0
+});
+
+export const evenSelect=selector({
+    key: "even",
+    get: (props)=>{
+        const count=props.get(countAtom);
+        return count % 2==0;
+    }
 });
