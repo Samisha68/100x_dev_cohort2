0e3d267dac07f9a1ec7205d70d8676c35b75852a
Samisha <108931301+Samisha68@users.noreply.github.com>
2024-08-12 00:35:48 +0530

    Create index.js

---
+++ paytm-main/backend/index.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const cors=require('cors');
+
+const mainRouter=require('./routes/index');
+const app=express();
+app.use(express.json());
+app.use(cors());
+app.use('api/v1',mainRouter);
+
+app.listen(3000,()=>{
+    console.log('server is running on port 3000');
+});
